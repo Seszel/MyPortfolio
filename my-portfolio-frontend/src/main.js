28fe7c3522721fcc39344464153d87079f01ae44
Joanna Adamczyk <joannaa251@gmail.com>
2024-01-14 23:35:04 +0100

    i18n folder structure reorganization

--- my-portfolio-frontend/src/main.js
+++ my-portfolio-frontend/src/main.js
@@ -5,7 +5,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import i18n from '../i18n/i18n'
+import i18n from './i18n/i18n'
 
 const app = createApp(App)
 
